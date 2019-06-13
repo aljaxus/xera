341fbf2fbb81a053701b6ccc703ae7e93565fb44
aljaxus <me@aljaxus.eu>
2019-06-13 03:57:06 +0200

    Created the image viewer component

---
+++ src/components/ImageViewer.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-layout row wrap>
+    <v-flex xs12 class="mt-1 centering">
+      <img
+        :src="source"
+        :lazy-src="source"
+        :alt="encodeURI(source)"
+      />
+    </v-flex>
+  </v-layout>
+</template>
+<script>
+export default {
+  name: 'ImageVidwer',
+  props: {
+    source: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    encodeURI: (source) => encodeURI(source),
+  }
+}
+</script>
+<style scoped>
+.centereing {
+  text-align: center;
+}
+</style>
